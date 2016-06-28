601a54ad013a97eaa77c57bdce7e0f1698cfaaa4
Daniel Metta <info@danielmetta.com.br>
2016-06-28 15:11:33 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-#Módulo Web Scrap para consulta de jogos do campeonato brasileiro
+# Módulo Web Scrap para consulta de jogos, tabela e rodadas do campeonato brasileiro
 
 [![npm](https://img.shields.io/npm/v/campeonato-brasileiro.svg)](https://www.npmjs.com/package/campeonato-brasileiro)
 [![npm](https://img.shields.io/npm/dm/campeonato-brasileiro.svg)](https://www.npmjs.com/package/campeonato-brasileiro)
 
-O **campeonato-brasileiro** é um módulo para consultar todos os jogos do campeonato brasileiro
+O **campeonato-brasileiro** é um módulo para consultar todos os jogos, tabela e rodadas do campeonato brasileiro
 
-O módulo retorna as informações dos jogos do campeonato brasileiro escolhido.
+O módulo retorna as informações dos jogos, tabela e rodadas do campeonato brasileiro escolhido.
 
 **npm install campeonato-brasileiro --save**
 
-## Exemplo prático
+## Exemplo prático - Jogos
 
 ```js
 
@@ -18,8 +18,8 @@ var cb = require('campeonato-brasileiro');
 var ano = 2016;
 var serie = 'a';
 
-cb.consult(ano, serie).then(function(tabela) {
-	console.log(tabela);
+cb.jogos(ano, serie).then(function(jogos) {
+	console.log(jogos);
 }, function(err){
 	console.log(err);
 });
@@ -44,6 +44,77 @@ cb.consult(ano, serie).then(function(tabela) {
 		"PFC"
 	]
 }]
+```
+
+### Objeto de erro
+
+```js
+{ error: 'Não foi possível retornar as informações!' }
+```
+
+
+## Exemplo prático - Tabela
+
+```js
+
+var cb = require('campeonato-brasileiro');
+
+var serie = 'a';
+
+cb.tabela(serie).then(function(tabela) {
+	console.log(tabela);
+}, function(err){
+	console.log(err);
+});
+```
+
+### Objeto de Retorno
+
+```js
+[{
+	nome: "Atlético",
+	pontos: "10",
+	jogos: "5",
+	vitorias: "2",
+	empates: "2",
+	derrotas: "1",
+	golsPro: "10",
+	golsContra: "4",
+	saldoGols: "6",
+	percentual: "66.3"
+}]
+```
+
+### Objeto de erro
+
+```js
+{ error: 'Não foi possível retornar as informações!' }
+```
+
+## Exemplo prático - Rodada Atual
+
+```js
+
+var cb = require('campeonato-brasileiro');
+
+var serie = 'a';
+
+cb.rodadaAtual(serie).then(function(rodada) {
+	console.log(rodada);
+}, function(err){
+	console.log(err);
+});
+```
+
+### Objeto de Retorno
+
+```js
+[{
+	 mandante: 'América-MG',
+	 placarMandante: 0,
+	 visitante: 'Corinthians',
+	 placarVisitante: 0
+ }]
 ```
 
 ### Objeto de erro
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('es6-promise').polyfill();
 var request = require('request')
 var cheerio = require('cheerio');
 
-exports.consult = function(ano, serie) {
+exports.jogos = function(ano, serie) {
   return new Promise(function(acept, error) {
     var options = {
       url: 'http://www.tabeladobrasileirao.net/' + ano + '/serie-' + serie,
@@ -49,6 +49,81 @@ exports.consult = function(ano, serie) {
             }
           });
           lista.push(produto);
+        });
+        acept(lista);
+      } else {
+        error({ error:"Não foi possível retornar as informações!" });
+      }
+    });
+  });
+};
+
+exports.tabela = function(serie) {
+  return new Promise(function(acept, error) {
+    var options = {
+      url: 'http://globoesporte.globo.com/futebol/brasileirao-serie-' + serie,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+      }
+    };
+    request(options, function(error, response, html) {
+      if(!error) {
+
+        var $ = cheerio.load(html);
+        var lista = [];
+
+        $('.tabela-times tbody tr').each(function() {
+          var item = $(this);
+          var time = {};
+          time.nome = item.find('.tabela-times-time-link').attr('title');
+          lista.push(time);
+        });
+        var x = 0;
+        $('.tabela-pontos tbody tr').each(function() {
+          var item = $(this);
+          lista[x].pontos = item.find('.tabela-pontos-ponto').text();
+          lista[x].jogos = item.find('.tabela-pontos-ponto').next().text();
+          lista[x].vitorias = item.find('.tabela-pontos-ponto').next().next().text();
+          lista[x].empates = item.find('.tabela-pontos-ponto').next().next().next().text();
+          lista[x].derrotas = item.find('.tabela-pontos-ponto').next().next().next().next().text();
+          lista[x].golsPro = item.find('.tabela-pontos-ponto').next().next().next().next().next().text();
+          lista[x].golsContra = item.find('.tabela-pontos-ponto').next().next().next().next().next().next().text();
+          lista[x].saldoGols = item.find('.tabela-pontos-ponto').next().next().next().next().next().next().next().text();
+          lista[x].percentual = item.find('.tabela-pontos-ponto').next().next().next().next().next().next().next().next().text();
+          x++;
+        });
+        acept(lista);
+      } else {
+        error({ error:"Não foi possível retornar as informações!" });
+      }
+    });
+  });
+};
+
+exports.rodadaAtual = function(serie) {
+  return new Promise(function(acept, error) {
+    var options = {
+      url: 'http://globoesporte.globo.com/futebol/brasileirao-serie-' + serie,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+      }
+    };
+    request(options, function(error, response, html) {
+      if(!error) {
+
+        var $ = cheerio.load(html);
+        var lista = [];
+
+        $('.lista-de-jogos-conteudo li').each(function() {
+          var rodada = {};
+          var item = $(this);
+          rodada.mandante = item.find('.placar-jogo-equipes').find('.placar-jogo-equipes-mandante').find('.placar-jogo-equipes-sigla').attr('title');
+          rodada.placarMandante = item.find('.placar-jogo-equipes').find('.placar-jogo-equipes-placar').find('.placar-jogo-equipes-placar-mandante').text();
+          rodada.visitante = item.find('.placar-jogo-equipes').find('.placar-jogo-equipes-visitante').find('.placar-jogo-equipes-sigla').attr('title');
+          rodada.placarVisitante = item.find('.placar-jogo-equipes').find('.placar-jogo-equipes-placar').find('.placar-jogo-equipes-placar-visitante').text();
+          if(!rodada.placarMandante) rodada.placarMandante = 0;
+          if(!rodada.placarVisitante) rodada.placarVisitante = 0;
+          lista.push(rodada);
         });
         acept(lista);
       } else {
